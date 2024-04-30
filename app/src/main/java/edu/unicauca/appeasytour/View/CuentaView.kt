9571b6bd53d538cda3338de06d73dec23c19211b
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2024-04-30 18:44:39 -0500

    se agrega vista en  Mi cuenta y se actializa la navegacion

---
+++ app/src/main/java/edu/unicauca/appeasytour/View/CuentaView.kt
@@ -0,0 +1,106 @@
+package edu.unicauca.appeasytour.View
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import edu.unicauca.appeasytour.R
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+
+@Composable
+fun cuentaView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Inicia sesión para gestionar tus reservas y planificar fácilmente tu próximo viaje")
+                Button(
+                    onClick = { /* Handle login here */ },
+                    modifier = Modifier
+                        .width(200.dp)
+                        .height(56.dp)
+                        .padding(5.dp)
+                ) {
+                    Text("Iniciar Sesión")
+                }
+            }
+        }
+
+        Button(
+            onClick = { /* Handle preferences here */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape= RectangleShape
+
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.settings),
+                    contentDescription = null
+                )
+                Text("Preferencias")
+            }
+        }
+        Button(
+            onClick = { /* Handle preferences here */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape= RectangleShape
+
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.circle_help),
+                    contentDescription = null
+                )
+                Text("Acerca de")
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+
+fun preCuenta(){
+    cuentaView()
+}
