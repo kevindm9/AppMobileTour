caa475a46ac2aa03caa942efc311477787ea0e9a
Ricardo Diaz <radiaz@unicauca.edu.co>
2024-05-03 00:07:43 -0500

    Arreglo de conflictos dentro de la navegacion

--- app/src/main/java/edu/unicauca/appeasytour/View/LoginScreen/CuentaNavigation.kt
+++ app/src/main/java/edu/unicauca/appeasytour/View/LoginScreen/CuentaNavigation.kt
@@ -2,13 +2,10 @@ package edu.unicauca.appeasytour.View.LoginScreen
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import edu.unicauca.appeasytour.View.CommentScreen.Views.AddScreen
-import edu.unicauca.appeasytour.View.CommentScreen.Views.CommentsScreen
-import edu.unicauca.appeasytour.View.CommentScreen.Views.UpdateScreen
+import edu.unicauca.appeasytour.View.about
+import edu.unicauca.appeasytour.View.cuentaView
 
 @Composable
 fun LoginNavigation(navController: NavHostController) {
@@ -22,11 +19,15 @@ fun LoginNavigation(navController: NavHostController) {
             LoginScreen(navController)
         }
 
+        composable(route = Screens.about.route){
+            about()
+        }
+
     }
 }
 
 sealed class Screens(val route:String){
     data object CuentaView:Screens("CuentaView")
     data object LoginScreen:Screens("loginScreen")
-
+    data object about:Screens("about")
 }
