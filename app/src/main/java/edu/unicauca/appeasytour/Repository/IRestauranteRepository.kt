db69653ce13eb53d55edd642c56d9f7bff8e450e
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2024-05-03 11:27:37 -0500

    Delete app directory

--- app/src/main/java/edu/unicauca/appeasytour/Repository/IRestauranteRepository.kt
+++
@@ -1,8 +0,0 @@
-package edu.unicauca.appeasytour.Repository
-
-import edu.unicauca.appeasytour.Model.RestaurantEntity
-
-interface IRestauranteRepository {
-    suspend fun getAllRestaurantes(): List<RestaurantEntity>
-    suspend fun insertRestaurante(restaurante: RestaurantEntity)
-}
--- app/src/main/java/edu/unicauca/appeasytour/View/CommentScreen/UseCase/UpdateComments.kt
+++
@@ -1,9 +0,0 @@
-package edu.unicauca.appeasytour.View.CommentScreen.UseCase
-
-import edu.unicauca.appeasytour.View.CommentScreen.Entity.Comment
-import edu.unicauca.appeasytour.View.CommentScreen.data.RepositoryIpml
-import javax.inject.Inject
-
-class UpdateComments @Inject constructor(private val respository: RepositoryIpml) {
-    suspend operator fun invoke(comment: Comment)=respository.updateComment(comment)
-}
