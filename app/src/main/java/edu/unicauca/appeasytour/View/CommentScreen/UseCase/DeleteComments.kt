453219f0e8935c65ee50a5a52fe8728b17764f83
kevindm9 <74603789+kevindm9@users.noreply.github.com>
2024-05-03 10:44:23 -0500

    fix arreglo de main

--- app/src/main/java/edu/unicauca/appeasytour/View/CommentScreen/UseCase/DeleteComments.kt
+++
@@ -1,8 +0,0 @@
-package edu.unicauca.appeasytour.View.CommentScreen.UseCase
-
-import edu.unicauca.appeasytour.View.CommentScreen.Entity.Comment
-import edu.unicauca.appeasytour.View.CommentScreen.data.RepositoryIpml
-
-class DeleteComments constructor(private val respository: RepositoryIpml) {
-    suspend operator fun invoke(comment: Comment)=respository.deleteComment(comment)
-}
--- app/src/main/java/edu/unicauca/appeasytour/View/CommentScreen/UseCase/GetComments.kt
+++
@@ -1,8 +0,0 @@
-package edu.unicauca.appeasytour.View.CommentScreen.UseCase
-
-import edu.unicauca.appeasytour.View.CommentScreen.data.RepositoryIpml
-import javax.inject.Inject
-
-class GetComments @Inject constructor(private val  repository: RepositoryIpml){
-    operator fun invoke()= repository.getComment()
-}
